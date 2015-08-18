d772ba1808eadcc24794a038fe60a477df4d0293
Jonas <jonasrydenhag@users.noreply.github.com>
2015-08-19 07:27:04 +0900

    Initial Commit

---
+++ Photo Meta/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Photo Meta
+//
+//  Created by Jonas Rydenhag on 2015-08-19.
+//  Copyright (c) 2015 Yasai, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Photo Meta/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Photo Meta
+//
+//  Created by Jonas Rydenhag on 2015-08-19.
+//  Copyright (c) 2015 Yasai, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
