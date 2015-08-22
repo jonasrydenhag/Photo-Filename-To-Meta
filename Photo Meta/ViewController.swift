c2341db97cee64f140ee2934e469dfa1939ae14c
Jonas <jonasrydenhag@users.noreply.github.com>
2015-08-22 14:58:19 +0900

    Indent with two spaces

--- Photo Meta/ViewController.swift
+++ Photo Meta/ViewController.swift
@@ -10,17 +10,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override var representedObject: AnyObject? {
+    didSet {
+      // Update the view, if already loaded.
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
+  }
 
 
 }
