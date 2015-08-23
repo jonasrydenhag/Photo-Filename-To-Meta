e4d59717fdf70e4b58066f76307fb550e8d4204f
Jonas <jonasrydenhag@users.noreply.github.com>
2015-08-23 16:58:11 +0900

    Iterates through folders, checks type of file and runs the script file by file

---
+++ Photo Meta/File.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  Photo Meta
+//
+//  Created by Jonas Rydenhag on 2015-08-23.
+//  Copyright (c) 2015 Yasai, Inc. All rights reserved.
+//
+
+import Foundation
+
+class File {
+  
+  private (set) var URL: NSURL
+  private (set) var path: String
+  var runner: ExifToolRunner
+  
+  init(fileURL: NSURL, runner: ExifToolRunner) {
+    self.URL = fileURL
+    self.path = fileURL.path!
+    self.runner = runner
+  }
+  
+  func process () {
+    runner.runTool(path)
+  }
+  
+  static func fileTypeConfomsTo(inPath: String, types: [CFString!]) -> Bool {
+    let pathExtension = inPath.pathExtension
+    let fileUTI: Unmanaged<CFString>! = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension, nil)
+    let fileUTICF: CFString! = fileUTI.takeRetainedValue()
+    
+    for kUTType in types {
+      if UTTypeConformsTo(fileUTICF, kUTType) != 0 {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+}
--- Photo Meta/ViewController.swift
+++ Photo Meta/ViewController.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
+  
+  private let fileManager = NSFileManager.defaultManager()
+  private let exifToolRunner = ExifToolRunner()
 
   @IBAction func openFileDialog(sender: NSButton) {
     choosePath()
@@ -33,20 +36,37 @@ class ViewController: NSViewController {
     myOpenDialog.runModal()
     
     // Get the path to the file chosen in the NSOpenPanel
-    var path = myOpenDialog.URL?.path
+    var pathURL = myOpenDialog.URL
     
     // Make sure that a path was chosen
-    if (path != nil) {
+    if let pathURL: NSURL = myOpenDialog.URL {
       var err = NSError?()
       
-      println(path)
-      let runner = ExifToolRunner()
-      runner.runTool(path!)
-      
-     if !(err != nil) {
-        NSLog(path!)
-     }
+      if !(err != nil) {
+        var isDir: ObjCBool = false
+        if fileManager.fileExistsAtPath(pathURL.path!, isDirectory:&isDir) && !isDir {
+          processFile(pathURL)
+          
+        } else {
+          let enumerator: NSDirectoryEnumerator? = fileManager.enumeratorAtURL(pathURL, includingPropertiesForKeys: nil, options: nil, errorHandler: nil)
+          
+          while let fileURL: NSURL = enumerator?.nextObject() as? NSURL {
+            processFile(fileURL)
+          }
+        }
+      }
+    }
+  }
+  
+  func processFile(URL: NSURL) {
+    if let path: String = URL.path {
+      var isDir: ObjCBool = false
+      if fileManager.fileExistsAtPath(path, isDirectory:&isDir) && !isDir {
+        if File.fileTypeConfomsTo(path, types: [kUTTypeJPEG, kUTTypeGIF, kUTTypeTIFF]) {
+          let file = File(fileURL: URL, runner: exifToolRunner)
+          file.process()
+        }
+      }
     }
   }
 }
-
